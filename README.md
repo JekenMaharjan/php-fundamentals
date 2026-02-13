2b16ad4053ca71cfd8e13fa199af0f97ebf64947
JekenMaharjan <maharjanjeken@gmail.com>
2026-02-14 00:34:35 +0545

    Different types of Operators topic complete

--- README.md
+++ README.md
@@ -24,13 +24,20 @@ php-fundamentals/
 
 1. Variables & Data Types
 2. Operators
+    - Arithmetic Operators
+    - Assignment Operators
+    - Comparison Operators
+    - Logical Operators
+    - Ternary Operator
+    - Null Coalescing Operator
+    - Spaceship Operator
 3. Conditionals
 4. Loops
 5. Functions
 6. Arrays
 7. Superglobals ($_GET, $_POST, $_SERVER)
 8. Form Handling
-9. Sessions
+9.  Sessions
 10. Basic Validation
 11. PDO Database Connection (Intro level)
 
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 // 1. Variables and Data Types
 
-// Variables
+// Declare Variables
 $name = "Jeken";
 $age = 25;
 $isMale = true;
@@ -14,7 +14,7 @@ echo "Age :" . $age . "<br>";
 echo "Male :" . ($isMale ? "Yes" : "No") . "<br>";
 echo "Salary :" . $salary . "<br><br>";
 
-// Type Checking
+// Data Type Checking
 var_dump($name);
 echo "<br>";
 var_dump($age);
@@ -27,4 +27,79 @@ var_dump($salary);
 
 // 2. Operators
 
+// a. Arithmetic Operators
+// Declare Variables
+$a = 5;
+$b = 12;
+
+// Output for different Operators
+echo "Addition: " . ($a + $b) . "<br>";
+echo "Subtraction: " . ($a - $b) . "<br>";
+echo "Multiplication: " . ($a * $b) . "<br>";
+echo "Division: " . ($a / $b) . "<br>";
+echo "Modulus: " . ($a % $b) . "<br>";
+
+// ------------------------
+
+// b. Assignment Operators
+$x = 3;
+
+$x += 2; // 5
+$x -= 1; // 4
+$x *= 2; // 8
+$x /= 4; // 2
+
+echo "The value of X is " . $x;
+
+// ------------------------
+
+// c. Comparison Operators
+$g = "5";
+$h = 5;
+
+var_dump($g == $h); // true
+var_dump($g === $h); // false
+var_dump($g != $h); // false
+var_dump($g !== $h); // true
+
+// ------------------------
+
+// d. Logical Operators
+$age = 25;
+$isVerified = true;
+
+if ($age > 18 && $isVerified) {
+    echo "Access Granted!";
+}
+
+if ($age < 18 || !$isVerified) {
+    echo "Access Denied!";
+}
+
+// ------------------------
+
+// e. Ternary Operator
+$marks = 60;
+
+$result = ($marks >= 60) ? "Pass" : "Fail";
+
+echo $result;
+
+// ------------------------
+
+// f. Null Coalescing Operator (Modern PHP)
+$username = $_GET['username'] ?? "Guest";
+
+echo $username;
+
+// ------------------------
+
+// g. Spaceship Operator (Advanced but Good to Know)
+echo 5 <=> 10; // -1
+echo 10 <=> 10; // 0
+echo 15 <=> 10; // 1
+
+// ====================================================
+
+// 3. Conditionals
 
