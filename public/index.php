c7c336871ba7b2d9b83974d05dde48a43e6561cb
JekenMaharjan <maharjanjeken@gmail.com>
2026-02-14 00:03:07 +0545

    Add public, config and src folders, then Variables and Data Types topic complete

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// 1. Variables and Data Types
+
+// Variables
+$name = "Jeken";
+$age = 25;
+$isMale = true;
+$salary = 50000.50;
+
+// Output
+echo "Name :" . $name . "<br>"; // . for string concatenation
+echo "Age :" . $age . "<br>";
+echo "Male :" . ($isMale ? "Yes" : "No") . "<br>";
+echo "Salary :" . $salary . "<br><br>";
+
+// Type Checking
+var_dump($name);
+echo "<br>";
+var_dump($age);
+echo "<br>";
+var_dump($isMale);
+echo "<br>";
+var_dump($salary);
+
+// ====================================================
+
+// 2. Operators
+
+
